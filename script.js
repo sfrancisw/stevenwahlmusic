4a8c88fd89b7e695b0412e8fdb4e9f2760a44a08
Steven <sfrancisw@gmail.com>
2023-05-24 15:54:52 +0200

    cmmit

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-// script.js
-
-window.onload = function() {
-    var musicPacksLink = document.querySelector('a[href="#music-packs"]');
-    musicPacksLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        var musicPacksElement = document.getElementById('music-packs');
-        musicPacksElement.scrollIntoView({ behavior: 'smooth' });
+// Smooth scroll animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
     });
+  });
+// Add this JavaScript to your script.js file or within a <script> tag in your HTML
 
-    var projectsLink = document.querySelector('a[href="#projects"]');
-    projectsLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        var projectsElement = document.getElementById('projects');
-        projectsElement.scrollIntoView({ behavior: 'smooth' });
-    });
-};
+// Get all the accordion item containers
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+// Add event listeners to the accordion item containers
+accordionItems.forEach((item) => {
+  const toggle = item.querySelector('.accordion-item-toggle');
+  const content = item.querySelector('.accordion-item-content');
+
+  toggle.addEventListener('click', () => {
+    if (item.classList.contains('active')) {
+      item.classList.remove('active');
+      content.style.maxHeight = null;
+    } else {
+      closeAccordionItems();
+      item.classList.add('active');
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+});
+
+// Function to close all accordion items
+function closeAccordionItems() {
+  accordionItems.forEach((item) => {
+    const content = item.querySelector('.accordion-item-content');
+    item.classList.remove('active');
+    content.style.maxHeight = null;
+  });
+}
